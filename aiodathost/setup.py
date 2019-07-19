8667015b02eafa128253bc8157ba4e7d704d8eb3
Ward <wpearce6@gmail.com>
2019-07-20 00:21:08 +1200

    v0.1

---
+++ aiodathost/setup.py
@@ -0,0 +1,38 @@
+from setuptools import setup, find_packages
+import sys, os
+
+here = os.path.abspath(os.path.dirname(__file__))
+README = open(os.path.join(here, 'README.rst')).read()
+NEWS = open(os.path.join(here, 'NEWS.txt')).read()
+
+
+version = '0.1'
+
+install_requires = [
+    # List your project dependencies here.
+    # For more details, see:
+    # http://packages.python.org/distribute/setuptools.html#declaring-dependencies
+]
+
+
+setup(name='aiodathost',
+    version=version,
+    description="Asynchronous wrapper for dathost's api.",
+    long_description=README + '\n\n' + NEWS,
+    classifiers=[
+      # Get strings from http://pypi.python.org/pypi?%3Aaction=list_classifiers
+    ],
+    keywords='dathost wrapper Asynchronous aio api',
+    author='Ward',
+    author_email='contact@districtnine.host',
+    url='https://districtnine.host',
+    license='GPL v3',
+    packages=find_packages('src'),
+    package_dir = {'': 'src'},include_package_data=True,
+    zip_safe=False,
+    install_requires=install_requires,
+    entry_points={
+        'console_scripts':
+            ['aiodathost=aiodathost:main']
+    }
+)
---
+++ aiodathost/src/aiodathost/aiodathost.py
@@ -0,0 +1,181 @@
+import aiohttp
+
+class dathost:
+    def __init__(self, username, password):
+        self.auth = aiohttp.BasicAuth(login=username, password=password)
+
+    async def start(server_id):
+        """ Starts given server id. """
+
+        session_object = aiohttp.ClientSession(auth=self.auth)
+        async with session_object as session:
+            async with session.post('https://dathost.net/api/0.1/game-servers/{}/start'.format(server_id)) as r:
+                data = r.status == 200
+
+        session_object.close
+
+        return data
+
+    async def stop(server_id):
+        """ Stops given server. """
+
+        session_object = aiohttp.ClientSession(auth=self.auth)
+        async with session_object as session:
+            async with session.post('https://dathost.net/api/0.1/game-servers/{}/stop'.format(server_id)) as r:
+                data = r.status == 200
+
+        session_object.close
+
+        return data
+
+    async def send_console(server_id, console_line):
+        """ Sends a line to the console. """
+
+        params = {'line': console_line}
+
+        session_object = aiohttp.ClientSession(auth=self.auth)
+        async with session_object as session:
+            async with session.post('https://dathost.net/api/0.1/game-servers/{}/console'.format(server_id), params=params) as r:
+                data = r.status == 200
+
+        session_object.close
+
+        return data
+
+    async def delete(server_id):
+        """ Deletes given server. """
+
+        session_object = aiohttp.ClientSession(auth=self.auth)
+        async with session_object as session:
+            async with session.delete('https://dathost.net/api/0.1/game-servers/{}'.format(server_id)) as r:
+                data = r.status == 200
+
+        session_object.close
+
+        return  data
+
+    async def delete_file(server_id, pathway, file_name):
+        """ Deletes file from given server. """
+
+        session_object = aiohttp.ClientSession(auth=self.auth)
+        async with session_object as session:
+            async with session.delete('https://dathost.net/api/0.1/game-servers/{}/files/{}{}'.format(server_id, pathway, file_name)) as r:
+                data = r.status == 200
+
+        session_object.close
+
+        return data
+
+    async def sync(server_id):
+        """ Syncs files between given server. """
+
+        session_object = aiohttp.ClientSession(auth=self.auth)
+        async with session_object as session:
+            async with session.post('https://dathost.net/api/0.1/game-servers/{}/sync-files'.format(server_id)) as r:
+                data = r.status == 200
+
+        session_object.close
+
+        return data
+
+    async def ftp_regenerate(server_id):
+        """ Generate a new random ftp password. """
+
+        session_object = aiohttp.ClientSession(auth=self.auth)
+        async with session_object as session:
+            async with session.post('https://dathost.net/api/0.1/game-servers/{}/regenerate-ftp-password'.format(server_id)) as r:
+                data = r.status == 200
+
+        session_object.close
+
+        return data
+
+    async def game_details(server_id):
+        """ Returns details on a game server. """
+
+        session_object = aiohttp.ClientSession(auth=self.auth)
+        async with session_object as session:
+            async with session.get('https://dathost.net/api/0.1/game-servers/{}'.format(server_id)) as r:
+                if r.status == 200:
+                    data = await r.json()
+                else:
+                    data = False
+
+        session_object.close
+
+        return data
+
+    async def download(server_id, pathway, file_name):
+        """ Downloads a file from the game server. """
+
+        session_object = aiohttp.ClientSession(auth=self.auth)
+        async with session_object as session:
+            async with session.get('https://dathost.net/api/0.1/game-servers/{}/files/{}{}'.format(server_id, pathway, file_name)) as r:
+                if r.status == 200:
+                    data = await r.read()
+                else:
+                    data = False
+
+        session_object.close
+
+        return data
+
+    async def account():
+        """ Returns account infomation. """
+
+        session_object = aiohttp.ClientSession(auth=self.auth)
+        async with session_object as session:
+            async with session.get('https://dathost.net/api/0.1/account') as r:
+                if r.status == 200:
+                    data = await r.json()
+                else:
+                    data = False
+
+        session_object.close
+
+        return data
+
+    async def details():
+        """ Returns full list of details about all the game servers. """
+
+        session_object = aiohttp.ClientSession(auth=self.auth)
+        async with session_object as session:
+            async with session.get('https://dathost.net/api/0.1/game-servers') as r:
+                if r.status == 200:
+                    data = await r.json()
+                else:
+                    data = False
+
+        session_object.close
+
+        return data
+
+    async def metrics(server_id):
+        """ Returns metricis saved by dathost about a game server. """
+
+        session_object = aiohttp.ClientSession(auth=self.auth)
+        async with session_object as session:
+            async with session.get('https://dathost.net/api/0.1/game-servers/{}/metrics'.format(server_id)) as r:
+                if r.status == 200:
+                    data = await r.json()
+                else:
+                    data = False
+
+        session_object.close
+
+        return data
+
+    async def clone(server_id):
+        """ Clones a game server and returns infomation on it. """
+
+        session_object = aiohttp.ClientSession(auth=self.auth)
+        async with session_object as session:
+            async with session.post('https://dathost.net/api/0.1/game-servers/{}/duplicate'.format(server_id)) as r:
+                if r.status == 200:
+                    data = await r.json()
+                else:
+                    data = False
+
+        session_object.close
+
+        return data
