f318fcf45dca7e456e69f3472891f5203511b225
Ward <wpearce6@gmail.com>
2019-09-18 16:32:28 +1200

    Fixes

---
+++ aiodathost/setup.py
@@ -0,0 +1,13 @@
+from setuptools import setup
+
+setup(
+    name='aiodathost',
+    version='0.3.2',
+    description='Asynchronous dathost API wrapper.',
+    url='http://districtnine.host',
+    author='DistrictNine.Host',
+    author_email='contact@districtnine.host',
+    license='GPL v3',
+    packages=['aiodathost'],
+    zip_safe=False
+)
