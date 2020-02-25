77fef772b991a2e2f8944b5444c8190fb6fcdbf0
WardPearce <wpearce6@gmail.com>
2020-02-25 18:51:23 +1300

    Update setup.py

--- setup.py
+++ setup.py
@@ -1,11 +1,16 @@
 from setuptools import setup
 
+def get_requirements():
+    with open("requirements.txt") as f:
+        return f.read().splitlines()
+
 setup(
     name='aiodathost',
     version='3.0.0',
     description='Asynchronous dathost API wrapper.',
     author='WardPearce',
     author_email='contact@districtnine.host',
+    install_requires=get_requirements(),
     license='GPL v3',
     packages=['aiodathost'],
     zip_safe=False
