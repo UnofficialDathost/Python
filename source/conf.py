da105093ddab28a5b0906a208145a7bbfe296ea7
WardPearce <wardpearce@protonmail.com>
2020-09-15 10:02:13 +1200

    Added async syntax to docs

--- source/conf.py
+++ source/conf.py
@@ -40,7 +40,8 @@ release = version
 extensions = [
     'sphinx.ext.autodoc',
     'sphinx.ext.coverage',
-    'sphinx.ext.napoleon'
+    'sphinx.ext.napoleon',
+    'sphinxcontrib_trio'
 ]
 
 # Add any paths that contain templates here, relative to this directory.
