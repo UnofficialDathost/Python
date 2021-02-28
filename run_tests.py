385992038a5edac2ac707906381c79be5a50944c
Ward <wardpearce@protonmail.com>
2021-02-28 13:51:23 +1300

    changes

--- run_tests.py
+++ run_tests.py
@@ -9,16 +9,16 @@ from dathost.tests.test_awaiting import TestAwaitingClient
 cli = argparse.ArgumentParser()
 
 cli.add_argument("--email", type=str, default="")
-cli.add_argument("--password", type=str, default="")
+cli.add_argument("--pw", type=str, default="")
 
 args = vars(cli.parse_args())
 
 
 if __name__ == "__main__":
     TestBlockingClient.email = args["email"]
-    TestBlockingClient.password = args["password"]
+    TestBlockingClient.password = args["pw"]
 
     TestAwaitingClient.email = args["email"]
-    TestAwaitingClient.password = args["password"]
+    TestAwaitingClient.password = args["pw"]
 
     unittest.main(argv=[sys.argv[0]])
